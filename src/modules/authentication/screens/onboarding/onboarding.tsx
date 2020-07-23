c114d9435a16ed92048a792fe3ea2e3c5fca8e32
alexey.orlov <orel10181@gmail.com>
2020-07-23 03:22:23 +0300

    feat(Onboarding): add slides

--- src/modules/authentication/screens/onboarding/onboarding.tsx
+++ src/modules/authentication/screens/onboarding/onboarding.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { Slide, SLIDE_HEIGHT } from './components';
+
+const { width } = Dimensions.get('window');
 
 export function Onboarding() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'cyan' }}>
-      <Text>Onboarding</Text>
+    <View style={styles.container}>
+      <View style={styles.slider}>
+        <ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+        >
+          <Slide label="Relaxed" />
+          <Slide label="Playful" isRight />
+          <Slide label="Excentric" />
+          <Slide label="Funky" isRight />
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <View style={{ ...StyleSheet.absoluteFillObject, backgroundColor: 'cyan' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'white', borderTopLeftRadius: 75 }}></View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  slider: { height: SLIDE_HEIGHT, borderBottomRightRadius: 75, backgroundColor: 'cyan' },
+  footer: { flex: 1 },
+});
